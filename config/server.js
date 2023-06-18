b907b8492c32581dece6367957b81c7b3f646ea5
rajendra <rajendra.prasad@tothenew.com>
2023-06-18 20:29:56 +0530

    add: csp added

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
+const setCustomCSPHeader = (ctx) => {
+  ctx.set('Content-Security-Policy', "connect-src 'self' http://localhost:1337");
+  ctx.set('Content-Security-Policy', "connect-src 'self' http://ec2-54-197-99-25.compute-1.amazonaws.com:1337");
+  ctx.set('Content-Security-Policy', "connect-src 'self' http://54.197.99.25:1337");
+};
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -6,5 +12,13 @@ module.exports = ({ env }) => ({
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  middleware: {
+    settings: {
+      csp: {
+        enabled: true,
+        fn: setCustomCSPHeader,
+      },
+    },
   },
 });
