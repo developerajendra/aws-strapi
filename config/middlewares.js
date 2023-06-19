7e81816e036bbe819042fba0d616babf96854545
rajendra <rajendra.prasad@tothenew.com>
2023-06-19 13:19:34 +0530

    add: csp added

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,9 +8,5 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
-  async (ctx, next) => {
-    ctx.set('Content-Security-Policy', "default-src 'self'; connect-src 'self' https: http://localhost:1337; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'");
-    await next();
-  },
+  'strapi::public' 
 ];
--- config/server.js
+++ config/server.js
@@ -5,6 +5,8 @@ const setCustomCSPHeader = (ctx) => {
   ctx.set('Content-Security-Policy', "connect-src 'self' https://localhost:1337");
   ctx.set('Content-Security-Policy', "connect-src 'self' https://ec2-54-197-99-25.compute-1.amazonaws.com:1337");
   ctx.set('Content-Security-Policy', "connect-src 'self' https://54.85.37.217");
+  
+  ctx.set('Content-Security-Policy', "default-src 'self'; connect-src 'self' https: http://localhost:1337; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'");
 };
 
 module.exports = ({ env }) => ({
